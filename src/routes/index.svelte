342610a609b66f682b81e272401ad80784871682
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2022-07-25 15:50:57 +0100

    Adding get all conditions

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -54,6 +54,16 @@
 				value += entry;
 			});
 	};
+
+	const getAllConditions = () => {
+		fetch('http://localhost:4444/conditions/')
+			.then((response) => response.json())
+			.then((data) => {
+				let entry = 'Request for all conditions:\n';
+				entry += stringifyResults(data.entry);
+				value += entry;
+			});
+	};
 </script>
 
 <h1>FHIR Server Analytics</h1>
@@ -66,7 +76,8 @@
 	<button on:click|once={getAllMedication}>Get all medication</button>
 	<button on:click|once={getAllAppointments}>Get all Appointments</button>
 	<button on:click|once={getAllImmunisations}>Get all Immunisations</button>
-	<!---also need medication administration, condsitions, diagnostic reprots, specimen (test result)-->
+	<button on:click|once={getAllConditions}>Get all Conditions</button>
+	<!---also need medication administration, diagnostic reprots, specimen (test result)-->
 </div>
 
 <textarea bind:value readonly />
